371cdaea8f64ed9bbda04517405e9d1744427e3c
fatihkacarf13 <fatihkacar@f13entertainment.com>
2022-09-21 03:36:43 +0300

    Refactor: CylinderObstacle

--- Assets/_GAME/Scripts/CylinderObstacle.cs
+++ Assets/_GAME/Scripts/CylinderObstacle.cs
@@ -18,11 +18,16 @@ public class CylinderObstacle : MonoBehaviour
     [SerializeField] private Color _color4;
 
     private float _defHealth;
+    private float _scaleDown;
+    private float _diff;
 
     private void Awake()
     {
         UpdateText();
+        UpdateColorNScale();
         _defHealth = _health;
+        _diff = transform.localScale.x - 0.75f;
+        _scaleDown = _diff / _defHealth;
 
     }
     private void OnTriggerEnter(Collider other)
@@ -33,27 +38,29 @@ public class CylinderObstacle : MonoBehaviour
             if (_health > 0)
             {
                 _health--;
-                UpdateColorNScale();
-                DamagaScale();
+                DamagaAnim();
                 UpdateText();
+                UpdateColor();
+                transform.localScale -=  Vector3.one*_scaleDown;
                 if (_health == 0) Destroy(gameObject);
             }
 
         }
     }
 
-    private void DamagaScale()
+    private void DamagaAnim()
     {
         _model.transform.DOScale(new Vector3(1.025f, 1.025f, 1.025f), 0.02f).OnComplete((() =>
           {
               _model.transform.DOScale(new Vector3(1, 1, 1), 0.01f);
           }));
     }
+
 #if UNITY_EDITOR
     [Button]
     private void UpdateColorNScale()
     {
-        if (_health <= 40)
+        if (_health > 30)
         {
             _mesh.material.color = _color4;
             transform.localScale = new Vector3(1.75f, 1.375f, 1.75f);
@@ -77,45 +84,17 @@ public class CylinderObstacle : MonoBehaviour
             transform.localScale = new Vector3(1f, 1f, 1f);
         }
 
-
     }
 #endif
 
-    //private void UpdateColorNScale()
-    //{
-    //    if (_health <= 40)
-    //    {
-    //        _mesh.material.DOColor(_color4,0.25f);
-    //        transform.DOScale(new Vector3(1.75f, 1.375f, 1.75f), 0.25f);
-    //    }
-
-    //    if (_health <= 30)
-    //    {
-    //        _mesh.material.DOColor(_color3, 0.25f);
-    //        transform.DOScale(new Vector3(1.5f, 1.25f, 1.5f), 0.25f);
-    //    }
-
-    //    if (_health <= 20)
-    //    {
-    //        _mesh.material.DOColor(_color2, 0.25f);
-    //        transform.DOScale(new Vector3(1.25f, 1.125f, 1.25f), 0.25f);
-    //    }
-
-    //    if (_health <= 10)
-    //    {
-    //        _mesh.material.DOColor(_color1, 0.25f);
-    //        transform.DOScale(new Vector3(1f, 1f, 1f),0.25f);
-    //    }
-
-
-    //}
-
-
-    private void ScaleDown()
+    private void UpdateColor()
     {
-        //_model.transform.DOScale(new Vector3(_health / _defHealth, _health / _defHealth, _health / _defHealth), 0.25f);
-    }
+        if (_health > 30) _mesh.material.DOColor(_color4,0.25f);
+        if (_health <= 30) _mesh.material.DOColor(_color3, 0.25f);
+        if (_health <= 20) _mesh.material.DOColor(_color2, 0.25f);
+        if (_health <= 10) _mesh.material.DOColor(_color1, 0.25f);
 
+    }
     private void UpdateText()
     {
         _tmp.text = "" + _health;
